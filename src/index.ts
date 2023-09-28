8eabddc1b245bd34ee658aad7dc3c82204949a82
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-28 20:38:46 +0200

    Create App class, add #main with example code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import { MiniBackup } from "./mini-backup";
+
+class App {
+  static main(): void {
+    new MiniBackup()
+      .findFiles("spiders-password-db.kdbx", ["D:\\", "E:\\"])
+      .then((results) => {
+        console.log("Results!", results);
+      });
+  }
+}
+
+App.main();
