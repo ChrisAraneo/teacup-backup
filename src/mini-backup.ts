8143750e57b70bc0b0c468a28c73c061723b563b
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-28 21:18:11 +0200

    Implement #encryptTexts

--- src/mini-backup.ts
+++ src/mini-backup.ts
@@ -1,5 +1,6 @@
-import find from "find";
+import CryptoAES from "crypto-js/aes";
 import fileBase64 from "file-base64";
+import find from "find";
 
 export class MiniBackup {
   async findFiles(
@@ -33,6 +34,16 @@ export class MiniBackup {
         });
       })
     ) as Promise<string[]>;
+  }
+
+  async encryptTexts(texts: string[], secretKey: string): Promise<string[]> {
+    return Promise.all(
+      texts.map((text: string) => this.encryptText(text, secretKey))
+    );
+  }
+
+  private async encryptText(text: string, secretKey: string): Promise<string> {
+    return CryptoAES.encrypt(text, secretKey).toString();
   }
 
   private async findFile(
