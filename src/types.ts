993d41cd2aef069e3bfdefcc933f8b6c615a3d90
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-29 19:53:16 +0200

    Remove unused types

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,3 @@
-export type File = {
-  path: string;
-  content: any;
-};
-
-export type TextFile = {
-  path: string;
-  content: string;
-};
-
 export type Base64File = {
   path: string;
   content: string;
