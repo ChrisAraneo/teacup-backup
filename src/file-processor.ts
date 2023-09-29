9f359f45fcace13d3dfc505509093f6ed36d1a5d
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-29 19:44:19 +0200

    Implement #readFilesToBase64 and #writeFilesFromBase64

--- src/file-processor.ts
+++ src/file-processor.ts
@@ -1,1 +1,58 @@
-export class FileProcessor {}
+import { Base64File } from "./types";
+var fs = require("node:fs");
+
+export class FileProcessor {
+  static async readFilesToBase64(files: string[]): Promise<Base64File[]> {
+    return Promise.all(
+      files.map((path: string) => {
+        return new Promise((resolve, reject) => {
+          this.readFileToBase64(path)
+            .then((data) => {
+              resolve({
+                path,
+                content: data,
+              });
+            })
+            .catch((error) => reject(error));
+        });
+      })
+    ) as Promise<Base64File[]>;
+  }
+
+  static async writeFilesFromBase64(filesInBase64: Base64File[]): Promise<void> {
+    await Promise.all(
+      filesInBase64.map((file: Base64File) => {
+        return this.writeFileFromBase64(file);
+      })
+    );
+  }
+
+  private static readFileToBase64(file: string): Promise<Base64File> {
+    return new Promise((resolve, reject) => {
+      fs.readFile(file, {encoding: 'base64'}, (error: unknown, data: string) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({
+            path: file,
+            content: data
+          });
+        }
+      });
+    });
+  }
+
+  private static writeFileFromBase64(file: Base64File): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const buffer = Buffer.from(file.content, 'base64');
+
+      fs.writeFile(file.path, buffer, (error: unknown) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(undefined);
+        }
+      });
+    });
+  }
+}
