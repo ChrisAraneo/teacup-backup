02b1ed87a1237bb0195a3517e90916408ddf3ac4
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-30 11:48:25 +0200

    Add #findFiles and #findFile

--- src/file-finder.ts
+++ src/file-finder.ts
@@ -1,1 +1,35 @@
-class FileFinder {}
+import find from "find";
+
+export class FileFinder {
+  static async findFile(
+    pattern: string | RegExp,
+    root: string = "C:\\"
+  ): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      find
+        .file(pattern, root, (result) => {
+          resolve(result);
+        })
+        .error((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  static async findFiles(
+    pattern: string | RegExp,
+    roots: string[] = ["C:\\"]
+  ): Promise<string[]> {
+    const result: string[] = [];
+
+    const promises = roots.map((root: string) => {
+      return this.findFile(pattern, root);
+    });
+
+    (await Promise.all(promises)).map((items: string[]) =>
+      result.push(...items)
+    );
+
+    return result;
+  }
+}
