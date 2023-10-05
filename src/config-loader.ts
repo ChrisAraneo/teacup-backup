140197824a8163689661fd517f2cae6134dd4025
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-05 23:24:42 +0200

    Implement #readConfigFile

--- src/config-loader.ts
+++ src/config-loader.ts
@@ -1,1 +1,24 @@
-export class ConfigLoader {}
+import Path from "path";
+import { FileProcessor } from "./file-processor";
+import { JsonFile } from "./models/json-file.class";
+
+export class ConfigLoader {
+  static async readConfigFile(): Promise<object> {
+    const path = Path.normalize(`${__dirname}/config.json`);
+    let config: JsonFile | undefined;
+
+    try {
+      config = await FileProcessor.readJsonFile(path);
+    } catch (error: unknown) {
+      throw Error(
+        "File config.json not found. Create config.json file in the application directory."
+      );
+    }
+
+    if (!config) {
+      throw Error("File config.json is empty or incorrect.");
+    }
+
+    return (config as JsonFile)?.getContent();
+  }
+}
