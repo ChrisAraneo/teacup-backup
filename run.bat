e4e05404b2969779d8a8137bac879637b8b99f35
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-08 23:00:58 +0200

    Add run batch file

---
+++ run.bat
@@ -0,0 +1,6 @@
+:: Windows
+:: Copy dist directory and this script to the autostart directory to run script on system start.
+::
+:: Copy to %AppData%\Microsoft\Windows\Start Menu\Programs\Startup
+:: or %ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup for all users
+node dist\index.js
