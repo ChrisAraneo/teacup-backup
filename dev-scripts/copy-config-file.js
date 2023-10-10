e89911d730115585648dd4aa5b895158bc0c4804
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-10 21:54:31 +0200

    Fix paths

--- dev-scripts/copy-config-file.js
+++ dev-scripts/copy-config-file.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-const source = path.normalize(`${__dirname}/src/config.json`);
-const destination = path.normalize(`${__dirname}/dist/config.json`);
+const source = path.normalize(`${__dirname}/../src/config.json`);
+const destination = path.normalize(`${__dirname}/../dist/config.json`);
 
 fs.copyFile(source, destination, (error) => {
   if (error) {
