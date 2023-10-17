234881f6b7dcf7bc38712f41b1ca9ee8f7f2b0c3
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-17 23:39:00 +0200

    Implement read methods in Base64FileReader

--- src/file-system/base64-file-reader.class.ts
+++ src/file-system/base64-file-reader.class.ts
@@ -1,1 +1,30 @@
-class Base64FileReader {}
+import FS from "fs";
+import { Base64File } from "../models/base64-file.class";
+
+export class Base64FileReader {
+  static async readFiles(paths: string[]): Promise<Base64File[]> {
+    return Promise.all(paths.map((path) => this.readFile(path)));
+  }
+
+  static async readFile(path: string): Promise<Base64File> {
+    return new Promise((resolve, reject) => {
+      FS.stat(path, (error: unknown, stats) => {
+        if (error) {
+          reject(error);
+        } else {
+          FS.readFile(
+            path,
+            { encoding: "base64" },
+            (error: unknown, data: string) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(new Base64File(path, data, new Date(stats.mtime)));
+              }
+            }
+          );
+        }
+      });
+    });
+  }
+}
