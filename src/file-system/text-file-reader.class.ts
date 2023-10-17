f3e5babc962990247b64ee38de1000a87f92b244
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-17 23:45:56 +0200

    Implement read methods in TextFileReader

--- src/file-system/text-file-reader.class.ts
+++ src/file-system/text-file-reader.class.ts
@@ -1,1 +1,26 @@
-class TextFileReader {}
+import FS from "fs";
+import { TextFile } from "../models/text-file.class";
+
+export class TextFileReader {
+  static async readFiles(paths: string[]): Promise<TextFile[]> {
+    return Promise.all(paths.map((path: string) => this.readFile(path)));
+  }
+
+  static async readFile(path: string): Promise<TextFile> {
+    return new Promise((resolve, reject) => {
+      FS.stat(path, (error, stats) => {
+        if (error) {
+          reject(error);
+        } else {
+          FS.readFile(path, "utf8", (error: unknown, data: string) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(new TextFile(path, data, new Date(stats.mtime)));
+            }
+          });
+        }
+      });
+    });
+  }
+}
