091e1c5d81d684a73f31623ee772a26d70997ad4
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-18 22:11:37 +0200

    Implement #readFile

--- src/file-system/json-file-reader.class.ts
+++ src/file-system/json-file-reader.class.ts
@@ -1,1 +1,20 @@
-class JsonFileReader {}
+import { JsonFile } from "../models/json-file.class";
+import { FileReader, ReadFileResult } from "./file-reader.class";
+
+export class JsonFileReader extends FileReader<JsonFile> {
+  async readFile(path: string): Promise<JsonFile> {
+    return new Promise((resolve, reject) => {
+      this._readFile(path, "utf8")
+        .catch((error) => reject(error))
+        .then((result: ReadFileResult) => {
+          resolve(
+            new JsonFile(
+              result.path,
+              JSON.parse(result.data),
+              result.modifiedDate
+            )
+          );
+        });
+    });
+  }
+}
