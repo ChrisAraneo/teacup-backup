b715ddc26e0ecd77ba67bc88c0b9832d080ec558
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-19 17:00:31 +0200

    Create and implement FileWriter class

---
+++ src/file-system/file-writer.class.ts
@@ -0,0 +1,33 @@
+import FS from "fs";
+import { File } from "../models/file.class";
+
+export type ReadFileResult = {
+  path: string;
+  data: string;
+  modifiedDate: Date;
+};
+
+export abstract class FileWriter<T extends File<any>> {
+  constructor(protected encoding: BufferEncoding) {}
+
+  writeFile(file: T): Promise<void> {
+    return new Promise((resolve, reject) => {
+      FS.writeFile(
+        file.getPath(),
+        file.getContent(),
+        this.encoding,
+        (error: unknown) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(undefined);
+          }
+        }
+      );
+    });
+  }
+
+  async writeFiles(files: T[]): Promise<void> {
+    await Promise.all(files.map((file) => this.writeFile(file)));
+  }
+}
