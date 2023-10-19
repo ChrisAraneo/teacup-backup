b71e286a400e195170f40a379c61721bd9d527ad
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-19 17:01:54 +0200

    Create TextFileWriter

---
+++ src/file-system/text-file-writer.class.ts
@@ -0,0 +1,8 @@
+import { TextFile } from "../models/text-file.class";
+import { FileWriter } from "./file-writer.class";
+
+export class TextFileWriter extends FileWriter<TextFile> {
+  constructor() {
+    super("utf-8");
+  }
+}
