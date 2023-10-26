b24f53b6d68edc9ae8aacf1d17d3a4d7a904cfe0
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-26 19:36:21 +0200

    Add TextFileWriter #writeFiles unit test

--- src/file-system/text-file-writer.class.spec.ts
+++ src/file-system/text-file-writer.class.spec.ts
@@ -26,4 +26,18 @@ describe('TextFileWriter', () => {
     expect(call[2]).toBe('utf-8');
     expect(typeof call[3]).toBe('function');
   });
+
+  it('#writeFiles should write a text files', () => {
+    const files = [
+      new TextFile('test.txt', 'Hello World!', new Date('2023-10-26')),
+      new TextFile('test2.txt', 'Test', new Date('2023-10-26')),
+      new TextFile('test3.txt', 'Lorem ipsum', new Date('2023-10-26')),
+    ];
+    jest.spyOn(fs, 'writeFile');
+
+    writer.writeFiles(files);
+
+    const calls = jest.mocked(fs.writeFile).mock.calls;
+    expect(calls.length).toBe(3);
+  });
 });
