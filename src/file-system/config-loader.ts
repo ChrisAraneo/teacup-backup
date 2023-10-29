77bcb5d68a78b353e925f18fa650d561ac1d180f
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-29 16:34:19 +0100

    Move reader to property

--- src/file-system/config-loader.ts
+++ src/file-system/config-loader.ts
@@ -5,16 +5,19 @@ import { JsonFileReader } from './json-file-reader.class';
 import { FileSystem } from './file-system.class';
 
 export class ConfigLoader {
-  constructor(protected fileSystem: FileSystem) {}
+  private jsonFileReader: JsonFileReader;
+
+  constructor(protected fileSystem: FileSystem) {
+    this.jsonFileReader = new JsonFileReader(fileSystem);
+  }
 
   async readConfigFile(): Promise<object> {
-    const fileSystem = this.fileSystem;
     const currentDirectory = CurrentDirectoryProvider.getCurrentDirectory();
     const path = Path.normalize(`${currentDirectory}/config.json`);
     let config: JsonFile | undefined;
 
     try {
-      config = await new JsonFileReader(fileSystem).readFile(path); // TODO Move to property
+      config = await this.jsonFileReader.readFile(path);
     } catch (error: unknown) {
       throw Error(
         'File config.json not found. Create config.json file in the application directory.',
