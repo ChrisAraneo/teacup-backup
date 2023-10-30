4b0879cc9ce7214acc416c019eac531b9cb9636e
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-30 20:08:26 +0100

    Handle json files in mock

--- src/file-system/file-system.mock.class.ts
+++ src/file-system/file-system.mock.class.ts
@@ -7,7 +7,11 @@ export class FileSystemMock extends FileSystem {
   }
 
   stat(path: string, callback: (error: any, data?: any) => any): void {
-    if (['test.txt', 'test2.txt', 'test3.txt'].includes(path) || path.includes('config.json')) {
+    if (
+      this.isCorrectTextFile(path) ||
+      this.isCorrectJsonFile(path) ||
+      this.isCorrectConfigFile(path)
+    ) {
       callback(null, {
         mtime: '2023-10-27T21:33:39.661Z',
       });
@@ -18,9 +22,11 @@ export class FileSystemMock extends FileSystem {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   readFile(path: string, _options, callback: (error: any, data?: any) => any): void {
-    if (['test.txt', 'test2.txt', 'test3.txt'].includes(path)) {
+    if (this.isCorrectTextFile(path)) {
       callback(null, 'Hello World!');
-    } else if (path.includes('config.json')) {
+    } else if (this.isCorrectJsonFile(path)) {
+      callback(null, '{"name":"Joel"}');
+    } else if (this.isCorrectConfigFile(path)) {
       callback(
         null,
         '{"backupDirectory":"./backups","files":[{"filename":"this-is-example-filename.txt"}],"interval":3600,"mode":"backup","roots":["C:\\\\","D:\\\\","E:\\\\"]}',
@@ -28,5 +34,17 @@ export class FileSystemMock extends FileSystem {
     } else {
       callback('Error');
     }
+  }
+
+  private isCorrectTextFile(path: string): boolean {
+    return ['test.txt', 'test2.txt', 'test3.txt'].includes(path);
+  }
+
+  private isCorrectJsonFile(path: string): boolean {
+    return ['test.json', 'test2.json', 'test3.json'].includes(path);
+  }
+
+  private isCorrectConfigFile(path: string): boolean {
+    return path.includes('config.json');
   }
 }
