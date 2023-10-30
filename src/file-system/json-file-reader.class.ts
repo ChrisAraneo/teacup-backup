079b4ddb0d36b428d8ba2d4134767b663441d522
Krzysztof Pająk <chris.araneo@gmail.com>
2023-10-30 20:09:29 +0100

    Add minus sign to encoding

--- src/file-system/json-file-reader.class.ts
+++ src/file-system/json-file-reader.class.ts
@@ -5,7 +5,7 @@ import { ReadFileResult } from './read-file-result.type';
 export class JsonFileReader extends FileReader<JsonFile> {
   async readFile(path: string): Promise<JsonFile> {
     return new Promise((resolve, reject) => {
-      this._readFile(path, 'utf8')
+      this._readFile(path, 'utf-8')
         .catch((error) => reject(error))
         .then((result: ReadFileResult) => {
           resolve(new JsonFile(result.path, JSON.parse(result.data), result.modifiedDate));
