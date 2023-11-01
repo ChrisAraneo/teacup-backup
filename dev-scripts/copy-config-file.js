607051e915e2643688d22bb323cd0a526f05335d
Krzysztof Pająk <chris.araneo@gmail.com>
2023-11-01 16:53:58 +0100

    Format code

--- dev-scripts/copy-config-file.js
+++ dev-scripts/copy-config-file.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const source = path.normalize(`${__dirname}/../src/config.json`);
 const destination = path.normalize(`${__dirname}/../dist/config.json`);
