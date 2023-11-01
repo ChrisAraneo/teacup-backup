020cb58be9aec54b6a7e912453e258408d40e89e
Krzysztof Pająk <chris.araneo@gmail.com>
2023-11-01 16:56:16 +0100

    Implement #info, add ignoring warnings options

--- src/utils/logger.class.ts
+++ src/utils/logger.class.ts
@@ -1,1 +1,28 @@
-export class Logger {}
+import winston from 'winston';
+import { Logger as WinstonLogger } from 'winston';
+
+export class Logger {
+  private logger: WinstonLogger;
+
+  constructor(private areWarningsIgnored: boolean = true) {
+    this.areWarningsIgnored && this.ignoreWarnings();
+
+    this.initialize();
+  }
+
+  info(message: string, ...meta: any[]): void {
+    this.logger.info(message, ...meta);
+  }
+
+  private initialize(): void {
+    this.logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.json(),
+      transports: [new winston.transports.Console({ format: winston.format.simple() })],
+    });
+  }
+
+  private ignoreWarnings(): void {
+    console.warn = (): undefined => {};
+  }
+}
