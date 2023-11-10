c7ba77eaaca4aff692f8fe7ba4cbd542f8a76ba6
Krzysztof Pająk <chris.araneo@gmail.com>
2023-11-10 18:28:50 +0100

    Add JsonFile unit tests

---
+++ src/models/json-file.class.spec.ts
@@ -0,0 +1,51 @@
+import { JsonFile } from './json-file.class';
+
+describe('JsonFile', () => {
+  it('instance should be created', async () => {
+    const file = new JsonFile('test.json', { name: 'Tommy' }, new Date('2023-11-10'));
+
+    expect(file).toBeInstanceOf(JsonFile);
+  });
+
+  it('#getPath should return correct path', async () => {
+    const file = new JsonFile('test.json', { name: 'Tommy' }, new Date('2023-11-10'));
+    const path = file.getPath();
+
+    expect(path).toBe('test.json');
+  });
+
+  it('#getFilename should return correct filename', async () => {
+    const file = new JsonFile('test.json', { name: 'Tommy' }, new Date('2023-11-10'));
+    const path = file.getFilename();
+
+    expect(path).toBe('test');
+  });
+
+  it('#getExtension should return correct extension', async () => {
+    const file = new JsonFile('test.json', { name: 'Tommy' }, new Date('2023-11-10'));
+    const extension = file.getExtension();
+
+    expect(extension).toBe('json');
+  });
+
+  it('#getContent should return correct file content', async () => {
+    const file = new JsonFile('test.json', { name: 'Tommy' }, new Date('2023-11-10'));
+    const content = file.getContent();
+
+    expect(content).toStrictEqual({ name: 'Tommy' });
+  });
+
+  it('#getHashValue should return correct hash value', async () => {
+    const file = new JsonFile('test.json', { name: 'Tommy' }, new Date('2023-11-10'));
+    const hash = file.getHashValue();
+
+    expect(hash).toBe('1441a7909c087dbbe7ce59881b9df8b9');
+  });
+
+  it('#getDate should return correct date', async () => {
+    const file = new JsonFile('test.json', { name: 'Tommy' }, new Date('2023-11-10'));
+    const date = file.getModifiedDate();
+
+    expect(date.toISOString()).toBe('2023-11-10T00:00:00.000Z');
+  });
+});
