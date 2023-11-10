3370dd38426b32f57f9e4822c11cc13f84297f60
Krzysztof Pająk <chris.araneo@gmail.com>
2023-11-10 18:22:32 +0100

    Add EncryptedFile unit tests

--- src/models/encrypted-file.class.spec.ts
+++ src/models/encrypted-file.class.spec.ts
@@ -11,6 +11,66 @@ beforeEach(() => {
 });
 
 describe('EncryptedFile', () => {
+  it('instance should be created', async () => {
+    const file = await lastValueFrom(EncryptedFile.fromEncryptedFile('test.mbe', fileSystem));
+
+    expect(file).toBeInstanceOf(EncryptedFile);
+  });
+
+  it('#getPath should return correct path', async () => {
+    const file = await lastValueFrom(
+      EncryptedFile.fromEncryptedFile('directory/test.mbe', fileSystem),
+    );
+    const path = file.getPath();
+
+    expect(path).toBe('directory/test.mbe');
+  });
+
+  it('#getFilename should return correct filename', async () => {
+    const file = await lastValueFrom(
+      EncryptedFile.fromEncryptedFile('directory/test.mbe', fileSystem),
+    );
+    const path = file.getFilename();
+
+    expect(path).toBe('test');
+  });
+
+  it('#getExtension should return correct extension', async () => {
+    const file = await lastValueFrom(
+      EncryptedFile.fromEncryptedFile('directory/test.mbe', fileSystem),
+    );
+    const extension = file.getExtension();
+
+    expect(extension).toBe('mbe');
+  });
+
+  it('#getContent should return correct file content', async () => {
+    const file = await lastValueFrom(
+      EncryptedFile.fromEncryptedFile('directory/test.mbe', fileSystem),
+    );
+    const content = file.getContent();
+
+    expect(content).toBe('U2FsdGVkX19B53TiyfRaPnNzSe5uo2K8dIO/fD5h+slCLO30KJAjw4HGKxqRBgGC');
+  });
+
+  it('#getHashValue should return correct hash value', async () => {
+    const file = await lastValueFrom(
+      EncryptedFile.fromEncryptedFile('directory/test.mbe', fileSystem),
+    );
+    const hash = file.getHashValue();
+
+    expect(hash).toBe('1569236e09c70c952c9316000ab6ff14');
+  });
+
+  it('#getDate should return correct date', async () => {
+    const file = await lastValueFrom(
+      EncryptedFile.fromEncryptedFile('directory/test.mbe', fileSystem),
+    );
+    const date = file.getModifiedDate();
+
+    expect(date.toISOString()).toBe('2023-10-27T21:33:39.661Z');
+  });
+
   it('#fromBase64File should create encrypted file', async () => {
     const encrypted = new Base64File('test.txt', 'Hello World!', new Date('2023-11-01'));
     const file = EncryptedFile.fromBase64File(encrypted, 'secret-key');
