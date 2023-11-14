e186bc1d4403bcda287528135a7daec93ab3fd10
Krzysztof Pająk <chris.araneo@gmail.com>
2023-11-14 20:47:59 +0100

    Create DirectoryCreator class

---
+++ src/file-system/directory-creator.class.ts
@@ -0,0 +1,27 @@
+import { FileSystem } from './file-system.class';
+import { Logger } from '../utils/logger.class';
+
+export class DirectoryCreator {
+  constructor(
+    private fileSystem: FileSystem,
+    private logger: Logger,
+  ) {}
+
+  createIfDoesntExist(directory: string): void {
+    if (!this.fileSystem.existsSync(directory)) {
+      this.logger.debug(`Creating directory: '${directory}'`);
+
+      this.fileSystem.mkdirSync(
+        directory,
+        { recursive: true },
+        (error: NodeJS.ErrnoException, path?: string) => {
+          if (error) {
+            throw Error("Can't create directory");
+          } else if (path) {
+            this.logger.debug('Created directory');
+          }
+        },
+      );
+    }
+  }
+}
