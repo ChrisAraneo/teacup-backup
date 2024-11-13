a59a9840c5ec4b236621b5b46967d33fa84b89fe
Krzysztof Pająk <chris.araneo@gmail.com>
2024-11-13 21:29:45 +0100

    refactor: create barrel file

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,102 +1,32 @@
-import { firstValueFrom, Observable } from 'rxjs';
-
-import { ConfigLoader } from './file-system/config-loader/config-loader.class';
-import { CurrentDirectory } from './file-system/current-directory/current-directory.class';
-import { FileSystem } from './file-system/file-system/file-system.class';
-import { Config } from './models/config.type';
-import { TeacupBackup } from './teacup-backup';
-import { IntervalFormatter } from './utils/interval-formatter.class';
-import { Logger } from './utils/logger.class';
-
-// Stryker disable all
-
-class App {
-  private static logger: Logger;
-  private static teacupBackup: TeacupBackup;
-  private static configLoader: ConfigLoader = new ConfigLoader(
-    new CurrentDirectory(),
-    new FileSystem(),
-  );
-  private static intervalFormatter: IntervalFormatter = new IntervalFormatter();
-
-  static async main(): Promise<void> {
-    const config: Config | undefined = (await firstValueFrom(
-      App.readConfigFile(),
-    ).catch((error) => {
-      new Logger('error').error(
-        JSON.stringify(error, Object.getOwnPropertyNames(error)),
-      );
-    })) as Config;
-
-    if (!config) {
-      return;
-    }
-
-    App.logger = new Logger(config['log-level']);
-    App.teacupBackup = new TeacupBackup(App.logger);
-
-    this.printTitleAndAscii('0.5.1');
-
-    this.teacupBackup.promptUserSecretKey();
-
-    const intervalInSeconds = +config.interval;
-
-    if (config.mode === 'backup') {
-      if (intervalInSeconds > 0) {
-        this.logger.info(
-          `Application will check specified files and perform backups on them every ${App.intervalFormatter.format(
-            intervalInSeconds * 1000,
-          )}`,
-        );
-
-        setInterval(() => {
-          this.teacupBackup.runBackupFlow(config);
-        }, intervalInSeconds * 1000);
-      }
-
-      this.teacupBackup.runBackupFlow(config);
-    } else if (config.mode === 'restore') {
-      this.teacupBackup.runRestoreFlow(config);
-    } else {
-      this.logger.error('Invalid mode');
-    }
-  }
-
-  private static readConfigFile(): Observable<object> {
-    return this.configLoader.readConfigFile();
-  }
-
-  private static printTitleAndAscii(version: string): void {
-    this.logger.info(`Teacup Backup - version ${version}
-
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⡇⣴⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⡟⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⠿⠋⢀⣼⣿⣿⡷⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⠀⠀⢀⣴⣿⣿⣿⡿⠀⢹⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⡟⠀⣰⣿⣿⣿⡿⠋⠀⣠⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⡟⠀⢰⣿⣿⣿⠋⠀⢠⣾⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣇⠀⠸⣿⣿⡇⠀⠀⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⡆⠀⠹⣿⣿⠀⠀⠻⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣬⣭⣷⣶⣶⣾⣿⣶⣶⣶⣮⣭⣤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢠⣶⣿⠿⠛⠛⠉⠉⢀⣀⣀⣀⣀⣀⣀⣀⣀⠀⢀⣀⡈⠉⠉⠛⠻⢿⣷⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣴⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣤⣤⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⣿⡿⠃⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⣾⡿⠘⣻⣿⡿⠿⣿⣿⣿⡇⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣇⠀⣀⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠇⣴⣿⠏⠀⠀⣸⣿⣸⡟⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡟⣸⣿⠁⠀⣠⣾⣿⣿⣿⠃⠀⠀⠀
-⠀⠀⠀⠀⠀⢀⣠⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣱⣿⣿⣶⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀
-⠀⠀⣠⣴⣾⠿⠟⠋⢩⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⡿⠿⠋⠙⢻⣿⣷⣤⡀⠀⠀⠀
-⢠⣾⡿⠋⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣯⣿⣿⠁⠀⠀⠀⠀⠀⠉⠉⠻⣿⣆⠀⠀
-⣼⣿⣦⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠠⠶⢰⣿⣿⣷⡄
-⠸⣿⣾⡇⠀⠀⠀⠀⠀⠉⠛⠻⢿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⣾⠿⠛⢿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡿⣿⡇
-⠀⠘⠻⣿⣶⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠃⠀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣾⣿⣿⣼⣿⠃
-⠀⠀⠀⠈⠙⠻⣿⣿⣶⣦⣤⣤⣤⣄⣀⣀⡀⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣶⣿⢿⣿⣽⣶⡿⠟⠁⠀
-⠀⠀⠀⠀⠀⠀⠈⠙⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⣤⣤⣤⣼⣿⣿⣿⣶⣿⣿⣿⣟⣻⣭⣵⣾⡿⠿⠛⠉⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⠛⠛⠻⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠛⠛⠛⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
-`);
-  }
-}
-
-App.main();
+export * from './crypto/encryption-result.type';
+export * from './crypto/file-decryptor.class';
+export * from './crypto/file-encryptor.class';
+export * from './file-system/config-loader/config-loader.class';
+export * from './file-system/config-loader/config-loader.consts';
+export * from './file-system/current-directory/current-directory.class';
+export * from './file-system/directory-creator/directory-creator.class';
+export * from './file-system/directory-creator/directory-creator.consts';
+export * from './file-system/directory-info/directory-info.class';
+export * from './file-system/file-finder/file-finder.class';
+export * from './file-system/file-finder/find-file-result.type';
+export * from './file-system/file-reader/base64-file-reader.class';
+export * from './file-system/file-reader/file-reader.class';
+export * from './file-system/file-reader/file-reader.consts';
+export * from './file-system/file-reader/json-file-reader.class';
+export * from './file-system/file-reader/read-file-error.type';
+export * from './file-system/file-reader/read-file-result.type';
+export * from './file-system/file-reader/read-file-result-status.enum';
+export * from './file-system/file-reader/text-file-reader.class';
+export * from './file-system/file-system/file-system.class';
+export * from './file-system/file-writer/base64-file-writer.class';
+export * from './file-system/file-writer/file-writer.class';
+export * from './file-system/file-writer/text-file-writer.class';
+export * from './ftp/ftp-client.class';
+export * from './models/base64-file.class';
+export * from './models/config.type';
+export * from './models/encrypted-file.class';
+export * from './models/file.class';
+export * from './models/json-file.class';
+export * from './models/text-file.class';
+export * from './utils/interval-formatter.class';
+export * from './utils/logger.class';
