645e99b399b857809d396d691034f66690829c0c
Krzysztof Pająk <chris.araneo@gmail.com>
2024-11-27 23:09:13 +0100

    feat(title): create Title component

---
+++ packages/cli/src/shared/components/title.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'ink';
+import React from 'react';
+
+export default function Title() {
+  return (
+    <Text>
+      🍵 Teacup Backup <Text color='gray'>(v0.5.3)</Text>
+    </Text>
+  );
+}
