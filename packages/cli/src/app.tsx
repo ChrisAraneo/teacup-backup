ec350f0e25b0a94293c7848f100f0e8b1822f7ac
Krzysztof Pająk <chris.araneo@gmail.com>
2024-12-03 18:48:34 +0100

    feat(app): handle backup page option

--- packages/cli/src/app.tsx
+++ packages/cli/src/app.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import Backup from './backup/backup.js';
 import MainMenu from './main-menu/main-menu.js';
 import { RootState } from './store/store.js';
 
 export default function App() {
   const page = useSelector<RootState>((state) => state.page.value);
 
-  return <>{page === 'main-menu' && <MainMenu></MainMenu>}</>;
+  return (
+    <>
+      {page === 'main-menu' && <MainMenu></MainMenu>}
+      {page === 'backup' && <Backup></Backup>}
+    </>
+  );
 }
