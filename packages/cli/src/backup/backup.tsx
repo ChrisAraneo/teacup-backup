fefefa4706482b0da82679b4c93cb48135164876
Krzysztof Pająk <chris.araneo@gmail.com>
2024-12-03 18:47:53 +0100

    feat(backup): create base backup page

--- packages/cli/src/backup/backup.tsx
+++ packages/cli/src/backup/backup.tsx
@@ -1,11 +1,11 @@
-import { Box, useInput } from 'ink';
+import { Box, Text, useInput } from 'ink';
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Action from '../shared/components/action.js';
 import Breadcrumbs from '../shared/components/breadcrumbs.js';
 import Title from '../shared/components/title.js';
-import { moveDown, moveUp, setLast } from '../store/cursor.slice.js';
+import { moveDown, moveUp, reset, setLast } from '../store/cursor.slice.js';
 import { setPage } from '../store/page.slice.js';
 
 export default function Backup() {
@@ -20,7 +20,8 @@ export default function Backup() {
   });
 
   useEffect(() => {
-    dispatch(setLast(2));
+    dispatch(setLast(8));
+    dispatch(reset());
   });
 
   return (
@@ -34,23 +35,86 @@ export default function Backup() {
       minHeight={30}>
       <Title></Title>
       <Breadcrumbs items={['Main menu', 'Backup']}></Breadcrumbs>
+      <Text color={'white'}>Configuration</Text>
+      <Box flexDirection='column' rowGap={0}>
+        <Text color={'grey'}>
+          Files to backup:{' '}
+          <Action
+            index={0}
+            onSelect={() => {
+              console.log('TODO');
+            }}>
+            Test 1
+          </Action>
+        </Text>
+        <Text color={'grey'}>
+          Discs to search:{' '}
+          <Action
+            index={1}
+            onSelect={() => {
+              console.log('TODO');
+            }}>
+            Test 1
+          </Action>
+        </Text>
+        <Text color={'grey'}>
+          Backup directory:{' '}
+          <Action
+            index={2}
+            onSelect={() => {
+              console.log('TODO');
+            }}>
+            Test 1
+          </Action>
+        </Text>
+        <Text color={'grey'}>
+          FTP upload:{' '}
+          <Action
+            index={3}
+            onSelect={() => {
+              console.log('TODO');
+            }}>
+            Test 1
+          </Action>
+        </Text>
+        <Text color={'grey'}>
+          Interval:{' '}
+          <Action
+            index={4}
+            onSelect={() => {
+              console.log('TODO');
+            }}>
+            Test 1
+          </Action>
+        </Text>
+        <Text color={'grey'}>
+          Log level:{' '}
+          <Action
+            index={5}
+            onSelect={() => {
+              console.log('TODO');
+            }}>
+            Test 1
+          </Action>
+        </Text>
+      </Box>
       <Box flexDirection='column'>
         <Action
-          index={0}
+          index={6}
           onSelect={() => {
             console.log('TODO');
           }}>
           Test 1
         </Action>
         <Action
-          index={1}
+          index={7}
           onSelect={() => {
             console.log('TODO');
           }}>
           Test 2
         </Action>
         <Action
-          index={2}
+          index={8}
           onSelect={() => {
             dispatch(setPage('main-menu'));
           }}>
