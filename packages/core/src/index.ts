e0e723cd148b900d63e7f8063284f00741b1b9f6
Krzysztof Pająk <chris.araneo@gmail.com>
2024-12-08 15:32:57 +0100

    fix: export LogLevel type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -27,6 +27,7 @@ export * from './models/config.type';
 export * from './models/encrypted-file.class';
 export * from './models/file.class';
 export * from './models/json-file.class';
+export * from './models/log-level.type';
 export * from './models/text-file.class';
 export * from './teacup-backup';
 export * from './utils/interval-formatter.class';
