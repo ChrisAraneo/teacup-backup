439f5a9064c66e88a875cef5a36aa52712ac2f5c
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-10 22:29:10 +0100

    fear(checkbox): remove internal state and use props value only

--- packages/cli/src/shared/components/checkbox.tsx
+++ packages/cli/src/shared/components/checkbox.tsx
@@ -1,5 +1,5 @@
 import { Text, useInput } from 'ink';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { RootState } from '../../store/store.js';
@@ -12,7 +12,6 @@ interface Props {
 
 export default function CheckBox(props: Props) {
   const { index, onChange } = props;
-  const [value, setValue] = useState<boolean>(props.value);
   const [active, setActive] = useState<boolean>(false);
   const [isLastKeyReturn, setIsLastKeyReturn] = useState<boolean>(false);
 
@@ -27,7 +26,7 @@ export default function CheckBox(props: Props) {
     }
 
     if (key.return && !isLastKeyReturn) {
-      setValue(!value);
+      onChange(!props.value);
       setIsLastKeyReturn(true);
     } else if (key.return && isLastKeyReturn) {
       setActive(false);
@@ -42,22 +41,18 @@ export default function CheckBox(props: Props) {
     }
 
     if (key.backspace || key.delete) {
-      setValue(false);
+      onChange(false);
     } else if (char || key.leftArrow || key.rightArrow) {
-      setValue(!value);
+      onChange(!props.value);
     }
   });
-
-  useEffect(() => {
-    onChange(value);
-  }, [value]);
 
   return (
     <Text
       color={active ? 'yellow' : cursor === index ? '' : 'white'}
       backgroundColor={cursor === index && !active ? 'white' : ''}>
       <Text>{'['}</Text>
-      {value ? (
+      {props.value ? (
         <Text inverse={active}>{'X'}</Text>
       ) : (
         <Text inverse={active}> </Text>
