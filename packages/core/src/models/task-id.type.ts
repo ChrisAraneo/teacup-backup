2677f136be06341776e2be03a4b194b37f25e1e3
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-12 22:30:13 +0100

    feat(task-id): create enum

---
+++ packages/core/src/models/task-id.type.ts
@@ -0,0 +1,8 @@
+export enum TaskId {
+  CreateDirectory = 'CREATE_DIRECTORY',
+  FindFiles = 'FIND_FILES',
+  EncryptFiles = 'ENCRYPT_FILES',
+  WriteEncryptedFiles = 'WRITE_ENCRYPTED_FILES',
+  UploadFtp = 'UPLOAD_FTP',
+  Finalize = 'FINALIZE',
+}
