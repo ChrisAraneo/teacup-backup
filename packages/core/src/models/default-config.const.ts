e9a7e3cd3b6d489c70faa50bf53578929aca0d3d
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-03 19:46:54 +0100

    feat(default-config): create const

---
+++ packages/core/src/models/default-config.const.ts
@@ -0,0 +1,20 @@
+import { Config } from './config.interface';
+
+const defaultConfig: Config = {
+  roots: ['C:\\', 'D:\\'],
+  files: [],
+  mode: 'backup',
+  backupDirectory: './backup',
+  interval: 60 * 60 * 1000,
+  'log-level': 'debug',
+  ftp: {
+    enabled: false,
+    host: '',
+    user: '',
+    password: '',
+    directory: 'backup',
+  },
+  secret: '',
+};
+
+export const DEFAULT_CONFIG = JSON.stringify(defaultConfig);
