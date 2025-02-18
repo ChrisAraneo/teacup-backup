ec5d81f0fa21208ae09a5b9f70c7112d7b4f55d3
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-18 21:20:27 +0100

    feat(store): writeConfig epic, combine epics

--- packages/cli/src/store/config.epic.ts
+++ packages/cli/src/store/config.epic.ts
@@ -1,14 +1,20 @@
 import { JsonFile } from '@chris.araneo/file-system';
 import { Config } from '@teacup-backup/core';
-import { delay, filter, first, map, mergeMap } from 'rxjs';
+import { combineEpics } from 'redux-observable';
+import { debounceTime, delay, filter, first, map, mergeMap } from 'rxjs';
 
 import { instance } from '../instance.js';
-import { loadConfig, readConfig } from './config.slice.js';
+import {
+  loadConfig,
+  noop,
+  readConfig as readConfigAction,
+} from './config.slice.js';
+import { setPage } from './page.slice.js';
 import { AppEpic } from './store.js';
 
-export const configEpic: AppEpic = (action, state) =>
+const readConfigEpic: AppEpic = (action, state) =>
   action.pipe(
-    filter(readConfig.match),
+    filter(readConfigAction.match),
     mergeMap(() =>
       state.pipe(
         filter((state) => !state.config.loaded),
@@ -32,3 +38,20 @@ export const configEpic: AppEpic = (action, state) =>
       ),
     ),
   );
+
+const writeConfigEpic: AppEpic = (action, state) =>
+  action.pipe(
+    filter(setPage.match),
+    mergeMap(() =>
+      state.pipe(
+        filter((state) => !!state.config.loaded),
+        debounceTime(500),
+        first(),
+        mergeMap((state) =>
+          instance.writeConfig(state.config.data).pipe(map(() => noop())),
+        ),
+      ),
+    ),
+  );
+
+export const configEpic = combineEpics(readConfigEpic, writeConfigEpic);
