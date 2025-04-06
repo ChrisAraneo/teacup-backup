17ae17c6299b9eaf560e0c44507f47faafa2916e
Krzysztof Pająk <chris.araneo@gmail.com>
2025-04-06 22:22:37 +0200

    feat(hint): change accent text to bold instead of yellow

--- packages/cli/src/shared/components/hint.tsx
+++ packages/cli/src/shared/components/hint.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 export default function Hint() {
   return (
     <Text color='gray'>
-      Hint: use the <Text color='yellow'>↑</Text>, <Text color='yellow'>↓</Text>{' '}
-      and <Text color='yellow'>Enter</Text> keys to navigate.
+      Hint: use the <Text bold>↑</Text>, <Text bold>↓</Text> and{' '}
+      <Text bold>Enter</Text> keys to navigate.
     </Text>
   );
 }
