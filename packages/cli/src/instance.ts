66bf8e3249aea25b4acc4f67f3adbc6b82aa4101
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-15 15:57:50 +0100

    feat: delete current cli package

--- packages/cli/src/instance.ts
+++
@@ -1,4 +0,0 @@
-import { Logger } from '@chris.araneo/logger';
-import { TeacupBackup } from '@teacup-backup/core';
-
-export const instance = new TeacupBackup(new Logger());
--- packages/cli/src/shared/models/page.ts
+++
@@ -1,7 +0,0 @@
-export type Page =
-  | 'main-menu'
-  | 'backup'
-  | 'backup/edit-files'
-  | 'backup/edit-roots'
-  | 'backup/select-log-level'
-  | 'backup/progress';
